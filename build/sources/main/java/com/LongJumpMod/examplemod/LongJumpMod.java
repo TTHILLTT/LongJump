63d5c32f58c12435cbbc0833b73ae8451b2232b8
考锡琪 <kaoxiqi@qq.com>
2025-05-03 16:49:27 +0800

    Upload mod flies

---
+++ build/sources/main/java/com/LongJumpMod/examplemod/LongJumpMod.java
@@ -0,0 +1,25 @@
+package com.example.longjumpmod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingJumpEvent;
+
+@Mod(modid = "longjumpmod", version = "1.0")
+public class LongJumpMod {
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onPlayerJump(LivingJumpEvent event) {
+        if (event.entityLiving instanceof net.minecraft.entity.player.EntityPlayer) {
+            double horizontalMultiplier = 4.0; // Experimental value, adjust as needed
+            event.entityLiving.motionX *= horizontalMultiplier;
+            event.entityLiving.motionZ *= horizontalMultiplier;
+        }
+    }
+}
